8ee172c52acc8b27ac8a6ac7224c8dac620f3f85
Almuhanad-1 <almuhanad.alnihmy@gmail.com>
2023-08-13 11:56:24 +0300

    feat: create Header component

---
+++ src/features/ui/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Logo } from './parts/Logo'
+import { SingIn } from './parts/Signin'
+
+export const Header = () => (
+  <header>
+    <Logo />
+    <SingIn />
+  </header>
+)
---
+++ src/features/ui/components/Header/parts/Signin/index.tsx
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+export const SingIn = () => (
+  <Link href="/login">
+    Already have an account? <b>sign in</b>
+  </Link>
+)
